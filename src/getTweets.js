6d6471907c3615f93d0a2317683a57de24a8f3f1
Huangxin <maggiewangsjtu@gmail.com>
2014-11-27 19:24:49 -0500

    add comments

--- src/getTweets.js
+++ src/getTweets.js
@@ -1,5 +1,12 @@
+
+//import package `node-weibo`, 
+//note: make sure the path and file name is correct
 var Weibo = require('../../node-weibo');
 
+
+
+//request parameters: 
+//note: see sina API for the meaning of the parameters http://open.weibo.com/wiki/%E5%BE%AE%E5%8D%9AAPI
 var para = {
    "source": Weibo.appKey.appKey,
    "screen_name": 'CS小吃货',
@@ -9,15 +16,16 @@ var para = {
 console.log(para);
 
 
-// Weibo.Statuses.public_timeline(para, function(data){
-//    console.log(data);
-// });
 
+//get user weibo
+//para: parameters
+//function(data){}: call back function
 Weibo.Statuses.user_timeline(para, function(data){
 	console.log(data);
 	getWeibo(data.statuses);
 });
 
+//output tweets
 function getWeibo(statuses){
 	//console.log(statuses.length)
 	for (var i = statuses.length - 1; i >= 0; i--) {
